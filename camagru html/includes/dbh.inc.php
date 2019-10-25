aab8ad83d485b91b612a703cf6b2f300e2e729cc
amaquena <alvin1king@gmail.com>
2019-10-25 17:25:25 +0200

    files for converted to html for easy editing with live server

---
+++ camagru html/includes/dbh.inc.php
@@ -0,0 +1,34 @@
+<?php
+// $servername = "localhost";
+// $dbUsername = "root";
+// $dbpwd = "admins";
+// $dbName = "camagru_users";
+
+try {
+	$conn = new PDO("mysql:host=localhost;dbname=camagru_users", "root", "admins");
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	// insert data into the users table of the database using PDO
+	//$stmt = $conn->prepare("INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?,?,?)");
+	//varables in order as in the prepare statement
+	//$stmt->execute([$?,$?,$?]);
+
+	// Update table $? = varable placement
+	//$stmt = $conn->prepare("UPDATE user SET uidUsers where idUsers=1");
+	//$stmt->execute([$?]);
+
+	// Delete from a table
+	//$stmt = $conn->("DELETE FROM users WHERE idUsers=1");
+	//$stmt->execute();
+
+	//Get data from users table
+	//$stmt = $conn->prepare("SELECT uidUsers, email FROM users");
+	//$stmt->execute();
+	//while ($data = $stmt->fetch())
+	//	echo $data['uidUser'] . "->" . $data['email'] . "<br>";
+}
+catch (PDOException $e)
+{
+	die("Connection failed: " . $e->getMessage());
+}
+?>
