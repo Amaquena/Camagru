594608bd78bd4f8ec7680450c557a339a3bc7205
amaquena <alvin1king@gmail.com>
2019-11-18 17:24:22 +0200

    working on editor

--- editor.php
+++ editor.php
@@ -14,7 +14,32 @@
 		</div> -->
 
 		<div class="grid-container">
-			<div class="item1">stickers</div>
+			<div class="item1">
+				<!-- <select>
+					<option value="stickers/doggo1">doggo1</option>
+					<option value="stickers/frame1">frame1</option>
+					<option value="stickers/frame2">frame2</option>
+					<option value="stickers/frame3">frame3</option>
+					<option value="stickers/frame4">frame4</option>
+					<option value="stickers/frame5">frame5</option>
+					<option value="stickers/frame6">frame6</option>
+					<option value="stickers/frame7">frame7</option>
+					<option value="stickers/frame8">frame8</option>
+					<option value="stickers/frame9">frame9</option>
+					<option value="stickers/frame10">frame10</option>
+					<option value="stickers/frame11">frame11</option>
+					<option value="stickers/frame12">frame12</option>
+					<option value="stickers/frame13">frame13</option>
+				</select> -->
+				<?php
+					$dirname = "stickers/";
+					$images = glob($dirname . "*.png");
+
+					foreach ($images as $image) {
+						echo '<a href="#"><img src="' . $image . '" /></a><br />';
+					}
+					?>
+			</div>
 			<div class="item2">
 				<video id="video" autoplay></video>
 				<button id="snap">capture</button>
@@ -23,13 +48,15 @@
 			</div>
 			<div class="item4">
 				<canvas id="canvas" width="640" height="480"></canvas>
-				<button id="upload">Upload</button>
+				<button id="upload" onclick="save_image()">Upload</button>
+			</div>
+			<div class="item5">
+				<form enctype="multipart/form-data" method="post" name="changer">
+					<input name="image" accept="image/jpeg" type="file">
+					<input value="Submit" type="button">
+				</form>
 			</div>
 		</div>
-		<!-- 
-	
-		 -->
-
 	<?php
 	}
 	?>
--- functions/webcam.js
+++ functions/webcam.js
@@ -5,20 +5,25 @@ const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const snap = document.getElementById("snap");
 
-navigator.mediaDevices
-  .getUserMedia({ video: true, audio: false })
-  .then(function (stream) {
-    video.srcObject = stream;
-    video.play();
-  })
-  .catch(function (err) {
-    console.log("An error occurred! " + err);
-  });
+// navigator.mediaDevices
+//   .getUserMedia({ video: true, audio: false })
+//   .then(function (stream) {
+//     video.srcObject = stream;
+//     video.play();
+//   })
+//   .catch(function (err) {
+//     console.log("An error occurred! " + err);
+//   });
 
 // Draw image
 var context = canvas.getContext("2d");
 snap.addEventListener("click", function () {
-  context.drawImage(video, 0, 0, width, height);
-  var data = canvas.toDataURL();
-  console.log(data);
+	context.drawImage(video, 0, 0, width, height);
 });
+
+function save_image() {
+	var img = new Image();
+	img.src = canvas.toDataURL();
+	var btn = document.getElementById("upload");
+	btn.value = img.src;
+}
